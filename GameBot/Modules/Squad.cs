68f6ee50c88c4a6e7ea7494fc19e86cbd4ca81fd
TankMissile <trevans1@gmail.com>
2018-02-05 09:45:11 -0600

    Added Squad and Skuado

---
+++ GameBot/Modules/Squad.cs
@@ -0,0 +1,41 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBot.Modules
+{
+    class Squad : ModuleBase<SocketCommandContext>
+    {
+        [Command("squad")]
+        [Alias("assemble")]
+        [Summary("Assemble the squad!")]
+        public async Task SquadAsync([Remainder] string arg = "")
+        {
+            if(arg == "")
+            {
+                await ReplyAsync("SQUAD, ASSEMBLE!!!", true);
+            }
+            else
+            {
+                await ReplyAsync(arg.ToUpper() + ", ASSEMBLE!!!");
+            }
+        }
+
+        [Command("skuado")]
+        [Alias("assembru")]
+        [Summary("Assemble the skuado!")]
+        public async Task SkuadoAsync([Remainder] string arg = "")
+        {
+            if (arg == "")
+            {
+                await ReplyAsync("SQKUADO, ASSEMBRUUUUU!!!", true);
+            }
+            else
+            {
+                await ReplyAsync(arg.ToUpper() + ", ASSEMBRUUUU!!!");
+            }
+        }
+    }
+}
