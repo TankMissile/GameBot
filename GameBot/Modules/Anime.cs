420aaaca9a0abcbdbd9e58c821065760afbd16c0
TankMissile <trevans1@gmail.com>
2018-10-04 17:40:33 -0500

    Add help text and free search to anime commands

--- GameBot/Modules/Anime.cs
+++ GameBot/Modules/Anime.cs
@@ -14,12 +14,29 @@ namespace GameBot.Modules
     public class Anime : ModuleBase<SocketCommandContext>
     {
         private delegate string Query(HtmlNode item);
+        private string premiumCR = "filter=premium";
+        private string freeCR = "filter=free";
 
         [Command("all")]
+        [Summary("Provides a list of this week's new anime")]
         public async Task AllAnimeAsync()
         {
             //Get the list of anime
-            var page = await getCRPage();
+            var page = await getCRPage(premiumCR);
+
+            List<String> shows = getCRAttributes(page, ".release", getCRName);
+
+            string msg = String.Join("\n", shows.ToArray());
+            await ReplyAsync(msg);
+        }
+
+        [Command("all free")]
+        [Alias("free all")]
+        [Summary("Provides a list of this week's new anime")]
+        public async Task AllFreeAnimeAsync()
+        {
+            //Get the list of anime
+            var page = await getCRPage(freeCR);
 
             List<String> shows = getCRAttributes(page, ".release", getCRName);
 
@@ -28,10 +45,24 @@ namespace GameBot.Modules
         }
 
         [Command]
+        [Summary("Provides a list of today's new anime with links to the new episode")]
         public async Task AnimeAsync()
         {
             //Get the list of anime
-            var page = await getCRPage();
+            var page = await getCRPage(premiumCR);
+
+            List<String> shows = getCRAttributes(page, ".today .release", getCRName, getCRLink);
+
+            string msg = String.Join("\n", shows.ToArray());
+            await ReplyAsync(msg);
+        }
+
+        [Command("free")]
+        [Summary("Provides a list of today's new free anime with links to the new episode")]
+        public async Task FreeAnimeAsync()
+        {
+            //Get the list of anime
+            var page = await getCRPage(freeCR);
 
             List<String> shows = getCRAttributes(page, ".today .release", getCRName, getCRLink);
 
@@ -62,10 +93,10 @@ namespace GameBot.Modules
         }
 
         //Gets the calendar page from crunchyroll, as html
-        private async Task<HtmlNode> getCRPage()
+        private async Task<HtmlNode> getCRPage(params string[] urlParams)
         {
             var web = new HtmlWeb();
-            var document = await web.LoadFromWebAsync("https://www.crunchyroll.com/simulcastcalendar?filter=premium");
+            var document = await web.LoadFromWebAsync("https://www.crunchyroll.com/simulcastcalendar?" + String.Join("&", urlParams));
             return document.DocumentNode;
         }
 
