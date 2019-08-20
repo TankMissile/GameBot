e680aa17f48dc08384d9d09ab8a69e4e70bc71f7
TankMissile <trevans1@gmail.com>
2019-08-19 22:46:07 -0500

    improvements and finish which command

--- GameBot/Modules/RNG.cs
+++ GameBot/Modules/RNG.cs
@@ -30,10 +30,18 @@ namespace GameBot.Modules
 
         [Command("rate")]
         [Summary("Rates something out of 10")]
-        public async Task RateAsync(string subject = "")
+        public async Task RateAsync([Remainder] string subject = "")
         {
             double rating = random.Next(0, 100) / 10.0;
-            await ReplyAsync("I rate that " + subject + (subject.Length > 0 ? " " : "") + rating + "/10.");
+            if(subject.Equals("me"))
+            {
+                await ReplyAsync("Hmmm... you're a " + rating + "/10.");
+                return;
+            }
+
+            await ReplyAsync("I rate " + (subject.ToLower().StartsWith("that ") || subject.ToLower().StartsWith("the")
+                    || subject.Contains("@") || subject.ToLower().EndsWith("s") ? "" : "that ")
+                + subject + (subject.Length > 0 ? " " : "") + rating + "/10.");
         }
 
         [Command("roll")]
@@ -56,12 +64,35 @@ namespace GameBot.Modules
             await ReplyAsync(msg);
         }
 
+        [Name("Error")]
+        [Summary("Provides various error messages")]
+        public class Error : ModuleBase<SocketCommandContext>
+        {
+            [Command("error")]
+            [Summary("Returns an error")]
+            public async Task ErrorAsync()
+            {
+                await ReplyAsync(GetRandomErrorMessage("Error!"));
+            }
+
+            public static string GetRandomErrorMessage(string preface = "")
+            {
+                return (preface + " " + GetRandomLineInFile("errors.txt")).Trim();
+            }
+
+            [Command("error add")]
+            [Summary("Adds a new error message")]
+            public async Task AddErrorAsync([Remainder] string error)
+            {
+                AppendStringToFile("error.txt", error);
+                await ReplyAsync("Error added!");
+            }
+        }
+
         [Group("food")]
         [Summary("Provides commands for selecting a place or thing to eat")]
         public class Food : ModuleBase<SocketCommandContext>
         {
-            int lastId = -1;
-
             [Command]
             public async Task FoodAsync()
             {
@@ -99,136 +130,12 @@ namespace GameBot.Modules
             }
         }
 
-        [Group("rip")]
-        public class Rip : ModuleBase<SocketCommandContext>
-        {
-            int lastId = -1;
-
-            [Command]
-            [Summary("Send off a fallen ally.")]
-            public async Task JokeAsync()
-            {
-                await ReplyAsync(GetRandomLineInFile(@"rip.txt", ref lastId), GameBot.TryUseTts(Context.User.Id));
-            }
-
-            [Command]
-            [Summary("Retrieve a specific rip, at the given index")]
-            public async Task GetJokeAsync(int i)
-            {
-                await ReplyAsync(GetLineInFile(@"rip.txt", i - 1), GameBot.TryUseTts(Context.User.Id));
-            }
-
-            [Command("add")]
-            [Summary("Add a rip to the list")]
-            public async Task AddJokeAsync([Remainder] string joke)
-            {
-                if (joke != "")
-                {
-                    AppendStringToFile(@"rip.txt", joke);
-                    await ReplyAsync("Added rip to list with ID " + (File.ReadLines(GameBot.GetPath(@"rip.txt")).Count()));
-                }
-                else
-                {
-                    await ReplyAsync("You didn't even say anything! Boooooo!");
-                }
-            }
-
-            [Command("help")]
-            [Summary("Provides help text on how to use the `rip` commands")]
-            public async Task JokeHelpAsync()
-            {
-                await ReplyAsync("usage:\n```" +
-                    "  +rip [id] : Tells a specific rip if ID is provided or a random one if left blank.\n" +
-                    "  +rip add [rip] : Adds [rip] to the rip list.  Multiple words and punctuation are okay.```");
-            }
-
-            [Command("list")]
-            [Summary("Sends a list of all available rip messages, as a private message to the requester")]
-            public async Task ListJokesAsync()
-            {
-                var dm = await Context.User.GetOrCreateDMChannelAsync();
-
-                string[] list = File.ReadAllLines(@"rip.txt");
-
-                string msg = "";
-                for (int i = 0; i < list.Length; i++)
-                {
-                    msg += (i + 1) + ". " + list[i] + "\n";
-                    if ((i + 1) % 30 == 0)
-                    {
-                        await dm.SendMessageAsync("```" + msg + "```");
-                        await Task.Delay(500);
-                        msg = "";
-                    }
-                }
-            }
-        }
-
-        [Group("joke")]
-        public class Joke : ModuleBase<SocketCommandContext>
-        {
-            int lastId = -1;
-
-            [Command]
-            [Summary("Tell an inside joke that normal people will need explained.")]
-            public async Task JokeAsync()
-            {
-                await ReplyAsync(GetRandomLineInFile(@"jokes.txt", ref lastId), GameBot.TryUseTts(Context.User.Id));
-            }
-
-            [Command]
-            [Summary("Retrieve a specific joke, at the given index")]
-            public async Task GetJokeAsync(int i)
-            {
-                await ReplyAsync(GetLineInFile(@"jokes.txt", i - 1), GameBot.TryUseTts(Context.User.Id));
-            }
-
-            [Command("add")]
-            [Summary("Add a joke to the list")]
-            public async Task AddJokeAsync([Remainder] string joke)
-            {
-                while (joke.StartsWith("+"))
-                {
-                    joke = joke.Substring(1);
-                }
-
-                if (joke != "")
-                {
-                    AppendStringToFile(@"jokes.txt", joke);
-                    await ReplyAsync("Added joke to list with ID " + (File.ReadLines(GameBot.GetPath(@"jokes.txt")).Count()));
-                }
-                else
-                {
-                    await ReplyAsync("You didn't even make a joke! Boooooo!");
-                }
-            }
-
-            [Command("help")]
-            [Summary("Provides help text on how to use the `joke` commands")]
-            public async Task JokeHelpAsync()
-            {
-                await ReplyAsync("usage:\n```" +
-                    "  +joke [id] : Tells a specific joke if ID is provided or a random one if left blank.\n" +
-                    "  +joke add [joke] : Adds [joke] to the joke list.  Multiple words and punctuation are okay.```");
-            }
-
-            [Command("list")]
-            [Summary("Sends a list of all available jokes, as a private message to the requester")]
-            public async Task ListJokesAsync()
-            {
-                var dm = await Context.User.GetOrCreateDMChannelAsync();
-
-                await ReplyAsync("Here's the current list of jokes.  Open it in something that shows you line numbers!");
-                await Context.Channel.SendFileAsync(GameBot.GetPath(@"jokes.txt"));
-            }
-        }
-
         [Group("insult")]
         public class Insult : ModuleBase<SocketCommandContext>
         {
             readonly string FILENAME = @"insults.txt";
 
-            int lastId = -1;
+            static int lastId = -1;
 
             [Command]
             [Summary("Insult someone savagely")]
@@ -282,30 +189,168 @@ namespace GameBot.Modules
                 await ReplyAsync("Here's the current list of insults.  Open it in something that shows you line numbers!");
                 await Context.Channel.SendFileAsync(GameBot.GetPath(FILENAME));
             }
+
+            [Command("which"), Alias("last")]
+            [Summary("Displays the ID of the last random insult")]
+            public async Task WhichInsultAsync()
+            {
+                if (lastId == -1)
+                {
+                    await ReplyAsync("I haven't told a random insult yet!");
+                    return;
+                }
+                await ReplyAsync("The last random insult was: " + lastId);
+            }
         }
 
-        [Name("Error")]
-        [Summary("Provides various error messages")]
-        public class Error : ModuleBase<SocketCommandContext>
+        [Group("joke")]
+        public class Joke : ModuleBase<SocketCommandContext>
         {
-            [Command("error")]
-            [Summary("Returns an error")]
-            public async Task ErrorAsync()
+            readonly string FILENAME = @"jokes.txt";
+
+            static int lastId = -1;
+
+            [Command]
+            [Summary("Tell an inside joke that normal people will need explained.")]
+            public async Task JokeAsync()
             {
-                await ReplyAsync(GetRandomErrorMessage("Error!"));
-            }
-            
-            public static string GetRandomErrorMessage(string preface = "")
-            {
-                return (preface + " " + GetRandomLineInFile("errors.txt")).Trim();
+                await ReplyAsync(GetRandomLineInFile(FILENAME, ref lastId), GameBot.TryUseTts(Context.User.Id));
             }
 
-            [Command("error add")]
-            [Summary("Adds a new error message")]
-            public async Task AddErrorAsync([Remainder] string error)
+            [Command]
+            [Summary("Retrieve a specific joke, at the given index")]
+            public async Task GetJokeAsync(int i)
             {
-                AppendStringToFile("error.txt", error);
-                await ReplyAsync("Error added!");
+                await ReplyAsync(GetLineInFile(FILENAME, i - 1), GameBot.TryUseTts(Context.User.Id));
+            }
+
+            [Command("add")]
+            [Summary("Add a joke to the list")]
+            public async Task AddJokeAsync([Remainder] string joke)
+            {
+                while (joke.StartsWith("+"))
+                {
+                    joke = joke.Substring(1);
+                }
+
+                if (joke != "")
+                {
+                    AppendStringToFile(FILENAME, joke);
+                    await ReplyAsync("Added joke to list with ID " + (File.ReadLines(GameBot.GetPath(@"jokes.txt")).Count()));
+                }
+                else
+                {
+                    await ReplyAsync("You didn't even make a joke! Boooooo!");
+                }
+            }
+
+            [Command("help")]
+            [Summary("Provides help text on how to use the `joke` commands")]
+            public async Task JokeHelpAsync()
+            {
+                await ReplyAsync("usage:\n```" +
+                    "  +joke [id] : Tells a specific joke if ID is provided or a random one if left blank.\n" +
+                    "  +joke add [joke] : Adds [joke] to the joke list.  Multiple words and punctuation are okay.```");
+            }
+
+            [Command("list")]
+            [Summary("Sends a list of all available jokes, as a private message to the requester")]
+            public async Task ListJokesAsync()
+            {
+                var dm = await Context.User.GetOrCreateDMChannelAsync();
+
+                await ReplyAsync("Here's the current list of jokes.  Open it in something that shows you line numbers!");
+                await Context.Channel.SendFileAsync(GameBot.GetPath(FILENAME));
+            }
+
+            [Command("which"), Alias("last")]
+            [Summary("Displays the ID of the last random joke")]
+            public async Task WhichJokeAsync()
+            {
+                if(lastId == -1)
+                {
+                    await ReplyAsync("I haven't told a random joke yet!");
+                    return;
+                }
+                await ReplyAsync("The last random joke was: " + lastId);
+            }
+        }
+
+        [Group("rip")]
+        public class Rip : ModuleBase<SocketCommandContext>
+        {
+            readonly string FILENAME = @"rip.txt";
+            static int lastId = -1;
+
+            [Command]
+            [Summary("Send off a fallen ally.")]
+            public async Task RipAsync()
+            {
+                await ReplyAsync(GetRandomLineInFile(FILENAME, ref lastId), GameBot.TryUseTts(Context.User.Id));
+            }
+
+            [Command]
+            [Summary("Retrieve a specific rip, at the given index")]
+            public async Task GetRipAsync(int i)
+            {
+                await ReplyAsync(GetLineInFile(FILENAME, i - 1), GameBot.TryUseTts(Context.User.Id));
+            }
+
+            [Command("add")]
+            [Summary("Add a rip to the list")]
+            public async Task AddRipAsync([Remainder] string joke)
+            {
+                if (joke != "")
+                {
+                    AppendStringToFile(FILENAME, joke);
+                    await ReplyAsync("Added rip to list with ID " + (File.ReadLines(GameBot.GetPath(@"rip.txt")).Count()));
+                }
+                else
+                {
+                    await ReplyAsync("You didn't even say anything! Boooooo!");
+                }
+            }
+
+            [Command("help")]
+            [Summary("Provides help text on how to use the `rip` commands")]
+            public async Task RipHelpAsync()
+            {
+                await ReplyAsync("usage:\n```" +
+                    "  +rip [id] : Tells a specific rip if ID is provided or a random one if left blank.\n" +
+                    "  +rip add [rip] : Adds [rip] to the rip list.  Multiple words and punctuation are okay.```");
+            }
+
+            [Command("list")]
+            [Summary("Sends a list of all available rip messages, as a private message to the requester")]
+            public async Task ListRipAsync()
+            {
+                var dm = await Context.User.GetOrCreateDMChannelAsync();
+
+                string[] list = File.ReadAllLines(GameBot.GetPath(FILENAME));
+
+                string msg = "";
+                for (int i = 0; i < list.Length; i++)
+                {
+                    msg += (i + 1) + ". " + list[i] + "\n";
+                    if ((i + 1) % 30 == 0)
+                    {
+                        await dm.SendMessageAsync("```" + msg + "```");
+                        await Task.Delay(500);
+                        msg = "";
+                    }
+                }
+            }
+
+            [Command("which"), Alias("last")]
+            [Summary("Displays the ID of the last random joke")]
+            public async Task WhichRipAsync()
+            {
+                if (lastId == -1)
+                {
+                    await ReplyAsync("I haven't killed anybody (yet)!");
+                    return;
+                }
+                await ReplyAsync("The last random rip was: " + lastId);
             }
         }
 
@@ -327,7 +372,7 @@ namespace GameBot.Modules
 
             int id = random.Next(options.Length);
             string msg = options[id];
-            lastId = id;
+            lastId = id + 1;
 
             return msg;
         }
